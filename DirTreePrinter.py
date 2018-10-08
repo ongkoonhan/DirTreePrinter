c2a30216d351a1e26fa851eda97791e39ae00695
ongkoonhan <koonhan.ong.sg@gmail.com>
2018-10-08 15:29:17 +0800

    DirTreePrinter.py: fixed logic in traversal_path, added printout for ignored entries;

--- DirTreePrinter.py
+++ DirTreePrinter.py
@@ -4,7 +4,7 @@ import string
 from collections import deque
 
 
-class _dir_tree_printer:
+class __dir_tree_printer:
 
     def __init__(self):
         self.print_list = ["root/"]
@@ -14,27 +14,28 @@ class _dir_tree_printer:
         self.bar_T = chr(9500)
 
 
-    def _should_ignore(self, entry):
+    def should_ignore(self, entry, path):
         for pat in self.ignore_list:
             if fnmatch.fnmatch(entry, pat):
+                print("-- ignored: {0} -> ignore pattern: {1}".format(path, pat))
                 return True
         return False
 
 
-    def _print_sub_dir(self, sub_dir_name, traversal_path):
+    def print_sub_dir(self, sub_dir_name, traversal_path):
         traversal_path = traversal_path[:-1]
         str_print = []
         for traversed in traversal_path:
             if traversed == "1":
                 str_print.append(self.bar_vert + " "*3)
             else:
-                str_print.append(" " *4)
+                str_print.append(" "*4)
         str_print.extend([self.bar_T + self.bar_horz*3, sub_dir_name])
         str_print = "".join(str_print)
         self.print_list.append(str_print)
 
 
-    def _print_files(self, files, traversal_path):
+    def print_files(self, files, traversal_path):
         for file in files:
             str_print = []
             for traversed in traversal_path:
@@ -47,7 +48,7 @@ class _dir_tree_printer:
             self.print_list.append(str_print)
 
 
-    def _walk_dir(self, dir, traversal_path=""):
+    def walk_dir(self, dir, traversal_path=""):
         files = []
         sub_dirs = deque([])
 
@@ -59,24 +60,26 @@ class _dir_tree_printer:
             elif entry.is_file():
                 files.append(name)
 
-        if len(sub_dirs) <= 1:
+        if len(sub_dirs) == 0:
             traversal_path += "0"
-            self._print_files(files, traversal_path)   # Print files
         else:
             traversal_path += "1"
-            self._print_files(files, traversal_path)   # Print files
+        self.print_files(files, traversal_path)  # Print files
 
-            while True:
+        while True:
+            if len(sub_dirs) > 0:
                 sub_dir = sub_dirs.popleft()
-                self._print_sub_dir(sub_dir, traversal_path)   # Print sub_dirs
+                self.print_sub_dir(sub_dir, traversal_path)  # Print sub_dirs
 
-                if len(sub_dirs) > 0:
-                    self._walk_dir(os.path.join(dir, sub_dir), traversal_path)  # Recursive DFS
-                else:
-                    break
+                if len(sub_dirs) == 0:  # Modify after printing (last sub_dir)
+                    traversal_path = traversal_path[:-1] + "0"
+
+                self.walk_dir(os.path.join(dir, sub_dir), traversal_path)  # Recursive DFS
+            else:
+                break
 
 
-    def _walk_dir_with_ignore(self, dir, traversal_path=""):
+    def walk_dir_with_ignore(self, dir, traversal_path=""):
         files = []
         sub_dirs = deque([])
 
@@ -84,33 +87,35 @@ class _dir_tree_printer:
             name = entry.name
             if entry.is_dir():
                 name += "/"
-                if self._should_ignore(name):
+                if self.should_ignore(name, os.path.join(dir, name)):
                     continue
                 sub_dirs.append(name)
             elif entry.is_file():
-                if self._should_ignore(name):
+                if self.should_ignore(name, os.path.join(dir, name)):
                     continue
                 files.append(name)
 
-        if len(sub_dirs) <= 1:
+        if len(sub_dirs) == 0:
             traversal_path += "0"
-            self._print_files(files, traversal_path)   # Print files
         else:
             traversal_path += "1"
-            self._print_files(files, traversal_path)   # Print files
+        self.print_files(files, traversal_path)  # Print files
 
-            while True:
+        while True:
+            if len(sub_dirs) > 0:
                 sub_dir = sub_dirs.popleft()
-                self._print_sub_dir(sub_dir, traversal_path)   # Print sub_dirs
+                self.print_sub_dir(sub_dir, traversal_path)   # Print sub_dirs
 
-                if len(sub_dirs) > 0:
-                    self._walk_dir_with_ignore(os.path.join(dir, sub_dir), traversal_path)  # Recursive DFS
-                else:
-                    break
+                if len(sub_dirs) == 0:  # Modify after printing (last sub_dir)
+                    traversal_path = traversal_path[:-1] + "0"
+
+                self.walk_dir_with_ignore(os.path.join(dir, sub_dir), traversal_path)  # Recursive DFS
+            else:
+                break
 
 
 def print_dir_tree(dir, outfile, ignorefile=None):
-    printer = _dir_tree_printer()
+    printer = __dir_tree_printer()
     outfile = os.path.join(outfile)
 
     if os.path.exists(outfile):
@@ -129,9 +134,9 @@ def print_dir_tree(dir, outfile, ignorefile=None):
 
     # Run walk
     if ignorefile is not None:
-        printer._walk_dir_with_ignore(dir)
+        printer.walk_dir_with_ignore(dir)
     else:
-        printer._walk_dir(dir)
+        printer.walk_dir(dir)
 
     # Print tree
     with open(outfile, "a", encoding="utf-8") as f:
