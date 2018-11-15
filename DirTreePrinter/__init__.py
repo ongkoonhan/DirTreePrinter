401745eae85f1a1caa96461b700d0d6b641ab880
ongkoonhan <koonhanong@kpmg.com.sg>
2018-11-15 15:20:13 +0800

    restructed as a package

---
+++ DirTreePrinter/__init__.py
@@ -0,0 +1,1 @@
+from .DirTreePrinter import print_dir_tree
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="DirTreePrinter",
+    version="0.0.1",
+    description="Print your directory structure, with gitignore",
+    author="Ong Koon Han",
+    packages=find_packages(),
+    python_requires=">=3",
+)
